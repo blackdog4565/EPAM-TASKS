c09d1e380e32aa3483e46e3cf2cc25c2b42bcf83
blackdog4565 <vic.akmaev@mail.ru>
2019-07-17 02:50:49 +0400

    +tasks

---
+++ task_2/task_2_5/Program.cs
@@ -0,0 +1,275 @@
+﻿using System;
+
+namespace task_2_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int exit = 0;
+
+            while (exit != 1)
+            {
+                int choiceAction = 0;
+
+                int newY, newD, newM, newExperience;
+                string newFirstName, newSecondName, newPatronymic, newPost;
+                
+                DateTime newBornDate = new DateTime();
+
+                var t = new Employee();
+
+                bool next_step = true;
+
+                Console.Write("Введите фамилию: ");
+                newSecondName = Console.ReadLine();
+
+                t.SecondName = newSecondName;
+
+                Console.Write("Введите имя: ");
+                newFirstName = Console.ReadLine();
+
+                t.FirstName = newFirstName;
+
+                Console.Write("Введите отчество: ");
+                newPatronymic = Console.ReadLine();
+
+                t.Patronymic = newPatronymic;
+
+                do
+                {
+                    Console.Write("Введите дату рождения:\nЧисло = ");
+                    int.TryParse(Console.ReadLine(), out newD);
+
+                    Console.Write("Введите дату рождения:\nМесяц = ");
+                    int.TryParse(Console.ReadLine(), out newM);
+
+                    Console.Write("Введите дату рождения:\nГод = ");
+                    int.TryParse(Console.ReadLine(), out newY);
+                    try
+                    {
+                        newBornDate = new DateTime(newY, newM, newD);
+
+                        if (newBornDate > DateTime.Today)
+                            throw new Exception("Введены неверные значения!");
+                        else
+                            next_step = true;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        next_step = false;
+                    }
+                } while (next_step == false);
+
+                t.BornDate = newBornDate;
+
+                Console.Write("Введите стаж (лет): ");
+                int.TryParse(Console.ReadLine(), out newExperience);
+
+                t.Experience = newExperience;
+
+                Console.Write("Введите должность: ");
+                newPost = Console.ReadLine();
+
+                t.Post = newPost;
+
+                Console.Write("\nРаботник создан (\"Опять работа?\")");
+
+                while (choiceAction < 6)
+                {
+                    Console.WriteLine("\nВыберите действие:\n\t1: Вывести инициалы работника\n\t2: Вывести дату рождения работника\n\t3: Вывести возраст работника\n\t4: Вывести стаж работника\n\t5: Вывести должность\n\t6: Ввести данные нового работника\n\t7: Выйти");
+
+                    int.TryParse(Console.ReadLine(), out choiceAction);
+
+                    switch (choiceAction)
+                    {
+                        case 1:
+                            t.ShowInitials();
+                            break;
+                        case 2:
+                            t.ShowBirth();
+                            break;
+                        case 3:
+                            t.ShowAge();
+                            break;
+                        case 4:
+                            t.ShowExperience();
+                            break;
+                        case 5:
+                            t.ShowPost();
+                            break;
+                        case 6:
+                            break;
+                        case 7:
+                            exit = 1;
+                            break;
+                        default:
+                            break;
+                    }
+
+                    if (choiceAction > 7 || choiceAction < 1)
+                        Console.WriteLine("Введены неверные параметры!");
+                }
+            }
+        }
+    }
+    class User
+    {
+        private string _fName;
+        private string _sName;
+        private string _patr;
+        private DateTime _bornDate;
+        public DateTime BornDate
+        {
+            get
+            {
+                return _bornDate;
+            }
+            set
+            {
+                _bornDate = value;
+            }
+        }
+        public string FirstName
+        {
+            get
+            {
+                return _fName;
+            }
+            set
+            {
+                if (value.Trim().Length != 0)
+                {
+                    if (value.Length > 1)
+                    {
+                        _fName = value.Substring(0, 1).ToUpper() + value.Substring(1, value.Length - 1).ToLower();
+                    }
+                    else
+                        _fName = value.ToUpper();
+
+                }
+                else
+                    throw new ArgumentException("Пустое поле!");
+            }
+        }
+        public string SecondName
+        {
+            get
+            {
+                return _sName;
+            }
+            set
+            {
+                if (value.Trim().Length != 0)
+                {
+                    if (value.Length > 1)
+                    {
+                        _sName = value.Substring(0, 1).ToUpper() + value.Substring(1, value.Length - 1).ToLower();
+                    }
+                    else
+                        _sName = value.ToUpper();
+
+                }
+                else
+                    throw new ArgumentException("Пустое поле!");
+            }
+        }
+        public string Patronymic
+        {
+            get
+            {
+                return _patr;
+            }
+            set
+            {
+                if (value.Length > 1)
+                {
+                    _patr = value.Substring(0, 1).ToUpper() + value.Substring(1, value.Length - 1).ToLower();
+                }
+                else
+                    _patr = value.ToUpper();
+            }
+        }
+        private int GetAge()
+        {
+            DateTime today = DateTime.Today;
+
+            int _age = today.Year - BornDate.Year;
+            if (BornDate > today.AddYears(-_age))
+            {
+                _age--;
+            }
+            return _age;
+
+        }
+        public void ShowBirth()
+        {
+            Console.WriteLine("{0:D}", BornDate);
+        }
+        public void ShowAge()
+        {
+            Console.WriteLine(GetAge());
+        }
+        public void ShowInitials()
+        {
+            Console.WriteLine("Имя: " + FirstName + "\nФамилия: " + SecondName + "\nОтчество: " + Patronymic);
+        }
+
+    }
+    class Employee : User
+    {
+        private int _experience;
+        private string _post;
+        public string Post
+        {
+            get
+            {
+                return _post;
+            }
+            set
+            {
+                if (value.Trim().Length != 0)
+                {
+                    if (value.Length > 1)
+                    {
+                        _post = value.Substring(0, 1).ToUpper() + value.Substring(1, value.Length - 1).ToLower();
+                    }
+                    else
+                        _post = value.ToUpper();
+
+                }
+                else
+                    throw new ArgumentException("Пустое поле!");
+            }
+        }
+        public int Experience
+        {
+            get
+            {
+                return _experience;
+            }
+            set
+            {
+                if (value >= 0)
+                    _experience = value;
+                else
+                    throw new ArgumentException("Не может быть отрицательного стажа.");
+            }
+        }
+        public void ShowExperience()
+        {
+            if (Experience == 1)
+                Console.WriteLine(Experience + " год");
+            else if (Experience >= 2 && Experience <= 4)
+                Console.WriteLine(Experience + " года");
+            else
+                Console.WriteLine(Experience + " лет");
+        }
+        public void ShowPost()
+        {
+            Console.WriteLine("Должность: " + Post);
+        }
+    }
+}
+
---
+++ task_2/task_2_6/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace task_2_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int exit = 0;
+
+            while (exit != 1)
+            {
+                int choiceAction = 0;
+
+                var c = new Ring();
+
+                Console.Write("Введите координаты центра кольца:\nX = ");
+                int.TryParse(Console.ReadLine(), out int newX);
+
+                Console.Write("Y = ");
+                int.TryParse(Console.ReadLine(), out int newY);
+
+                c.X = newX;
+                c.Y = newY;
+
+                Console.Write("\nВведите внешний радиус кольца:\nR = ");
+                int.TryParse(Console.ReadLine(), out int newR);
+
+                c.Radius = newR;
+
+                Console.Write("\nВведите внутренний радиус кольца:\nIR = ");
+                int.TryParse(Console.ReadLine(), out int newIR);
+
+                c.InnerRadius = newIR;
+
+                while (choiceAction < 3)
+                {
+                    Console.WriteLine("\nДля кольца с параметрами:\n\tX = " + c.X + "\n\tY = " + c.Y + "\n\tR = " + c.Radius + "(внешний радиус)\n\tIR = " + c.InnerRadius + "(внутренний радиус)\nВыберите действие:\n\t1: Вычислить толщину кольца\n\t2: Вычислить площадь кольца\n\t3: Ввести новые значения\n\t4: Выйти");
+
+                    int.TryParse(Console.ReadLine(), out choiceAction);
+
+                    switch (choiceAction)
+                    {
+                        case 1:
+                            Console.WriteLine("Толщина кольца = " + c.GetWidth());
+                            break;
+                        case 2:
+                            Console.WriteLine("Площадь кольца = " + c.GetSquare());
+                            break;
+                        case 3:
+                            break;
+                        case 4:
+                            exit = 1;
+                            break;
+                        default:
+                            break;
+                    }
+
+                    if (choiceAction > 4 || choiceAction < 1)
+                        Console.WriteLine("Введены неверные параметры!");
+                }
+            }
+        }
+    }
+
+    class RoundFigure
+    {
+        private int _r;
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public int Radius
+        {
+            get
+            {
+                return _r;
+            }
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentException("Радиус должен быть положительным.");
+                else
+                    _r = value;
+            }
+        }
+    }
+    class Ring : RoundFigure
+    {
+        private int _ir;
+        public int InnerRadius
+        {
+            get
+            {
+                return _ir;
+            }
+            set
+            {
+                if (value <= 0 || value > Radius)
+                    throw new ArgumentException("Внутренний радиус не может быть больше внншнего и не может быть неположительным.");
+                else
+                    _ir = value;
+            }
+        }
+        public double GetSquare() => Math.PI * (Radius * Radius - InnerRadius * InnerRadius);
+        public double GetWidth() => Radius - InnerRadius;
+    }
+}
