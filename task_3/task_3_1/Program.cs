007979b0c2b5b152b23d133f9a106d46beb161bf
blackdog4565 <vic.akmaev@mail.ru>
2019-07-25 22:03:52 +0400

    task_3

---
+++ task_3/task_3_1/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace task_3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество людей: ");
+            int.TryParse(Console.ReadLine(), out int countPeople);
+
+            List<int> People = new List<int>();
+
+            int random;
+            Random r = new Random();
+
+            random = r.Next(1, countPeople);
+            People.Add(random);
+
+            for (int i = 1; i < countPeople; i++)
+            {
+                Console.WriteLine(People.Contains(random));
+                random = r.Next(1, countPeople);
+
+                while (People.Contains(random))
+                {
+                    random = r.Next(1, countPeople + 1);
+                }
+                People.Add(random);
+            }
+
+            //for (int i = 0; i < countPeople; i++)
+            //{
+            //    People.Add(i + 1);
+            //}
+
+            foreach (int man in People)
+            {
+                Console.Write(man + " ");
+            }
+            Console.WriteLine("\n");
+
+            int iter = 1;
+
+            while (People.Count != 2)
+            {
+                People.Remove(People[iter % People.Count]);
+
+                iter++;
+
+                Console.Write("Оставшиеся люди: ");
+
+                foreach (int man in People)
+                {
+                    Console.Write(man + " ");
+                }
+                Console.WriteLine("\n");
+            }
+
+            if (countPeople % 2 == 0)
+            {
+                People.Remove(People[0]);
+            }
+            else
+            {
+                People.Remove(People[1]);
+            }
+
+            foreach (int man in People)
+            {
+                Console.WriteLine(man);
+            }
+        }
+    }
+}
---
+++ task_3/task_3_2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+namespace task_3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите текст: ");
+            string userText = Console.ReadLine();
+
+            char[] separators = new char[] { ' ', ',' };
+            string[] splitText = userText.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<string, int> wordFreq = new Dictionary<string, int> ();
+
+            string lowWord;
+
+            foreach (string word in splitText)
+            {
+                lowWord = word.ToLower();
+
+                if (wordFreq.ContainsKey(lowWord))
+                {
+                    wordFreq[lowWord]++;
+                }
+                else
+                {
+                    wordFreq.Add(lowWord, 1);
+                }
+            }
+
+            foreach (string word in wordFreq.Keys)
+            {
+                Console.WriteLine("Слово: " + word + "\n\rЧастота встречаемости: " + wordFreq[word] + "\n\r");
+            }
+        }
+    }
+}
